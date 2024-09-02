c195cda33c6158ee36bef85f17e5c001bf45b5f4
Gecher Emre <emre.gecher@indusoft.ru>
2024-09-02 08:36:49 +0300

    DataTampered

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,68 +3,81 @@ import React, { useEffect, useState } from "react";
 const API_URL = "http://localhost:8080";
 
 function App() {
-  const [data, setData] = useState<string>();
+    const [data, setData] = useState<string>("");
 
-  useEffect(() => {
-    getData();
-  }, []);
+    useEffect(() => {
+        getData();
+    }, []);
 
-  const getData = async () => {
-    const response = await fetch(API_URL);
-    const { data } = await response.json();
-    setData(data);
-  };
+    const getData = async () => {
+        const response = await fetch(API_URL);
+        const { value } = await response.json();
+        setData(value);
+    };
 
-  const updateData = async () => {
-    await fetch(API_URL, {
-      method: "POST",
-      body: JSON.stringify({ data }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
+    const updateData = async () => {
+        await fetch(API_URL, {
+            method: "POST",
+            body: JSON.stringify({ data }),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
 
-    await getData();
-  };
+        await getData();
+    };
 
-  const verifyData = async () => {
-    throw new Error("Not implemented");
-  };
+    const verifyData = async () => {
+        const response = await fetch(`${API_URL}/verify`, {
+            method: "POST",
+            body: JSON.stringify({ data }),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
 
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        position: "absolute",
-        padding: 0,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        gap: "20px",
-        fontSize: "30px",
-      }}
-    >
-      <div>Saved Data</div>
-      <input
-        style={{ fontSize: "30px" }}
-        type="text"
-        value={data}
-        onChange={(e) => setData(e.target.value)}
-      />
+        if (response.ok) {
+            alert("Data is intact"); 
+        } else {
+            alert("Data has been tampered with"); 
+        }
+    };
 
-      <div style={{ display: "flex", gap: "10px" }}>
-        <button style={{ fontSize: "20px" }} onClick={updateData}>
-          Update Data
-        </button>
-        <button style={{ fontSize: "20px" }} onClick={verifyData}>
-          Verify Data
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                position: "absolute",
+                padding: 0,
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                gap: "20px",
+                fontSize: "30px",
+            }}
+        >
+            <div>Saved Data</div>
+            <input
+                style={{ fontSize: "30px" }}
+                type="text"
+                value={data}
+                onChange={(e) => setData(e.target.value)}
+            />
+
+            <div style={{ display: "flex", gap: "10px" }}>
+                <button style={{ fontSize: "20px" }} onClick={updateData}>
+                    Update Data
+                </button>
+                <button style={{ fontSize: "20px" }} onClick={verifyData}>
+                    Verify Data
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
 
 const PORT = 8080;
 const app = express();
@@ -11,14 +12,29 @@ app.use(express.json());
 // Routes
 
 app.get("/", (req, res) => {
-  res.json(database);
+    res.json(database);
 });
 
 app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
+    // database.data = req.body.data;
+    const { data } = req.body;
+    const hash = crypto.createHash('sha256').update(data).digest('hex');
+    database.data = { value: data, hash };
+
+    res.sendStatus(200);
+});
+
+// New route to verify data integrity
+app.post("/verify", (req, res) => {
+    const { data } = req.body;
+    const hash = crypto.createHash('sha256').update(data).digest('hex');
+    if (hash == database.data.hash) {
+        res.sendStatus(200);
+    } else {
+        res.status(400).send("Data has been tampered with");
+    }
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+    console.log("Server running on port " + PORT);
 });
